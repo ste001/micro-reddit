e7f0f0c028f1e78f9fed8faa3fa404585826e105
ste001 <asso99@hotmail.com>
2019-01-27 17:42:40 +0100

    Add comments

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  validates :message, presence: true, length: {maximum: 1000}
+  validates :user_id, presence: true
+  validates :post_id, presence: true
+end
