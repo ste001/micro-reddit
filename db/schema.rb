fd6ecdfeac8b3d7be86cba1e73c81da1c34ede00
ste001 <asso99@hotmail.com>
2019-01-27 16:06:47 +0100

    Add User and Post models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_27_144636) do
+ActiveRecord::Schema.define(version: 2019_01_27_150028) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "message"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
