31550ecbf6a09ef1a0ceb62fc50a26ecefd838ee
ste001 <asso99@hotmail.com>
2019-01-27 16:30:39 +0100

    Add associations between Posts and Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {minimum: 6, maximum: 36},
             uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_27_150028) do
+ActiveRecord::Schema.define(version: 2019_01_27_152321) do
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
